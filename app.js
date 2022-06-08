86be1b6316c9760e602a02e4c2347fe50dfa5649
Nguyen Mau Hung <hungnmfx14861@funix.edu.vn>
2022-06-08 15:23:33 +0700

    commit LAB1.4 fix

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const server = http.createServer((req, res) => {
         res.write('<head><title>Enter Message</title></head>');
         res.write('<body><form action="/message" method="POST"><input type="text" name="message" /><button type="submit">Send</button></form></body>');
         res.write('</html>');
-        return res.end;
+        return res.end();
 
     }
     res.setHeader('Content-Type', 'text/html');
