91fe88083e9f5d91e8bd67fc5567d639d2ecbf3a
Nguyen Mau Hung <hungnmfx14861@funix.edu.vn>
2022-06-11 15:18:02 +0700

    commit LAB2.8

--- app.js
+++ app.js
@@ -11,4 +11,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).send("<h1>Page Not Found</h1>");
+})
+
+
 app.listen(3000);
