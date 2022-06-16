4e15c843bf2edf5f57ad812fedc3bfcb20851b40
Nguyen Mau Hung <hungnmfx14861@funix.edu.vn>
2022-06-16 15:53:57 +0700

    commit LAB3.8

--- app.js
+++ app.js
@@ -3,9 +3,17 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const expressHbs = require('express-handlebars');
+
 const app = express();
 
-app.engine('hbs', expressHbs());
+app.engine(
+    'hbs',
+    expressHbs({
+        layoutsDir: 'views/layouts/',
+        defaultLayout: 'main-layout',
+        extname: 'hbs'
+    })
+);
 app.set('view engine', 'hbs');
 app.set('views', 'views');
 
@@ -21,6 +29,5 @@ app.use(shopRoutes);
 app.use((req, res, next) => {
     res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
-
 
 app.listen(3000);
